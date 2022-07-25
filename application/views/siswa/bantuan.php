59f1885adce0438baf539f34433a9a94979a7487
kuriyantoadi <melatikuning212@gmail.com>
2022-07-26 00:17:19 +0700

    update 26 juli 2022, update 6

--- application/views/siswa/bantuan.php
+++ application/views/siswa/bantuan.php
@@ -5,9 +5,12 @@
         <h1 class="h3 mb-4 text-gray-800">Bantuan Cetak Sertifikat MPLS</h1>
         <!-- <h4 class="h3 mb-4 text-gray-800">Sistem Download Sertifikat MPLS SMK Negeri 1 Kragilan 2022</h4> -->
         <p>Sertifikat dapat dibantu untuk mencetak sesuai dengan standar dari sekolah, siswa tidak perlu cetak sendiri.</p>
-        <p>Ini hanya sifatnya tawaran bantuan saja, tidak diwajibkan, mungkin ada biaya untuk meminta bantuan mencetak sertifikat. Informasi lebih lanjut bisa hubungi kak Nisa kelas XII TKJ 2.
-            <a href="https://wa.link/buo1h7" class="btn btn-success btn-sm">Bantu Cetak Sertifikat</a>
-        </p>
+        <p>Ini hanya sifatnya tawaran bantuan saja, tidak diwajibkan, mungkin ada biaya untuk meminta bantuan mencetak sertifikat.</p>
+
+        <b>
+            Informasi lebih lanjut bisa hubungi kak Nisa kelas XII TKJ 2.
+        </b>
+        <a href="https://wa.link/buo1h7" class="btn btn-success btn-sm">Bantu Cetak Sertifikat</a>
 
 
     </div>
