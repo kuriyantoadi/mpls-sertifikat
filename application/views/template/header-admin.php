cb7b7d5ac886ccae77615d2902e6aa0194d0c5b6
kuriyantoadi <melatikuning212@gmail.com>
2022-07-26 20:58:29 +0700

    update 26 juli 2022

--- application/views/template/header-admin.php
+++ application/views/template/header-admin.php
@@ -35,7 +35,7 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= base_url() ?>Siswa">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= base_url() ?>Admin/dashboard">
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-lightbulb"></i>
                 </div>
